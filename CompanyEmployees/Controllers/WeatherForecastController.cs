274cb47cf08019925043b43f255e2dfceab09db3
Margarita Busarova <margarita.busarova13@gmail.com>
2025-03-30 16:39:16 +0300

    2

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -3,24 +3,32 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class WeatherForecastController : ControllerBase
     {
-        private ILoggerManager _logger;
-        public WeatherForecastController(ILoggerManager logger)
+        private readonly ILoggerManager _logger;
+        private readonly IRepositoryManager _repository;
+
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repository)
         {
-            
-        _logger = logger;
+            _logger = logger;
+            _repository = repository;
         }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<string>> Get()
         {
-            _logger.LogInfo("¬от информационное сообщение от нашего контроллера значений."); 
-            _logger.LogDebug("¬от отладочное сообщение от нашего контроллера значений."); 
-            _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений."); 
-            _logger.LogError("¬от сообщение об ошибке от нашего контроллера значений."); 
-        return new string[] { "value1", "value2" };
+            _logger.LogInfo("¬от информационное сообщение от нашего контроллера значений.");
+            _logger.LogDebug("¬от отладочное сообщение от нашего контроллера значений.");
+            _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений.");
+            _logger.LogError("¬от сообщение об ошибке от нашего контроллера значений.");
+
+            _repository.Company.AnyMethodFromCompanyRepository();
+            _repository.Employee.AnyMethodFromEmployeeRepository();
+
+            return new string[] { "value1", "value2" };
         }
     }
 }
+
