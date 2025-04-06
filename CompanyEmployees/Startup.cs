c70342990522e31fa66ec616d1fac4613289933a
Margarita Busarova <margarita.busarova13@gmail.com>
2025-04-06 13:57:10 +0300

    3

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -25,6 +25,7 @@ public class Startup
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        services.AddAutoMapper(typeof(Startup));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
