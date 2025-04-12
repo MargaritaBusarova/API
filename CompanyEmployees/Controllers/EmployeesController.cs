f8ba8b52aebfd8a81eadd67e82b56bb7f1650e2d
Margarita Busarova <margarita.busarova13@gmail.com>
2025-04-12 22:30:23 +0400

    lab10

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -29,6 +29,7 @@ namespace CompanyEmployees.Controllers
             _dataShaper = dataShaper;
         }
         [HttpGet]
+        [HttpHead]
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, [FromQuery] EmployeeParameters employeeParameters)
         {
             if (!employeeParameters.ValidAgeRange)
